828b1071add441f9c778c2951e3f847af2a10502
Jesse Olson <jolson@nagios.com>
2017-05-20 23:10:41 -0500

    misc improvements, related models together

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,7 @@
 class StoriesController < ApplicationController
 
+  before_action :ensure_login, only: [:new, :create]
+
   def index
     @story = Story.order('RANDOM()').first
   end
@@ -9,7 +11,8 @@ class StoriesController < ApplicationController
   end
 
   def create
-    @story = Story.new(story_params)
+    @story = @current_user.stories.build story_params
+#    @story = Story.new(story_params)
     if @story.save
       flash[:notice] = 'Story submission succeeded'
       redirect_to stories_path
